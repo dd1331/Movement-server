a47a8f2e0cc0aa745e68e09232addf9ca51f0d09
Chalie Choi <ddd1331@gmail.com>
2021-04-23 19:12:16 +0900

    fix

--- src/cache/cache.module.ts
+++ src/cache/cache.module.ts
@@ -6,7 +6,12 @@ import { CacheController } from './cache.controller';
 @Module({
   imports: [
     CacheModule.register({
-      store: redisStore,
+      // store: redisStore,
+      store: redisStore.create({
+        host: process.env.REDIS_HOST || 'localhost',
+        port: process.env.REDIS_PORT || 6379,
+        password: process.env.REDIS_PASSWORD,
+      }),
       // host: 'redis-server',
       // host: 'localhost',
       host: process.env.REDIS_HOST || 'localhost',
@@ -18,7 +23,12 @@ import { CacheController } from './cache.controller';
   exports: [
     CacheService,
     CacheModule.register({
-      store: redisStore,
+      // store: redisStore,
+      store: redisStore.create({
+        host: process.env.REDIS_HOST || 'localhost',
+        port: process.env.REDIS_PORT || 6379,
+        password: process.env.REDIS_PASSWORD,
+      }),
       // host: 'redis-server',
       // host: 'localhost',
       host: process.env.REDIS_HOST || 'localhost',
